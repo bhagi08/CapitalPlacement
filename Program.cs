ed2f207507bf82fe2a160926df93dd0456be0689
Bhagya Millewa <BhagyaM@fortude.co>
2024-05-22 23:00:32 +0530

    Starting-Project

---
+++ Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+
+
+app.Run();
+
+
